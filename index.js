b9ed6267f8b141d58d7285f7ad74925ef10aa697
Erik Dever <erikdever@hotmail.com>
2021-05-18 20:32:59 -0600

    Added Home page and Show page

--- index.js
+++ index.js
@@ -23,11 +23,22 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
     res.render('home')
 })
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'Backyard', description: 'Cheap camping' });
-    await camp.save();
-    res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
 })
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
+})
+
+
+// app.get('/makecampground', async (req, res) => {
+//     const camp = new Campground({ title: 'Backyard', description: 'Cheap camping' });
+//     await camp.save();
+//     res.send(camp)
+// })
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
