714a337bf8720f353986d5bbab11c4d6d6a24402
Erik Dever <erikdever@hotmail.com>
2021-06-10 21:45:56 -0600

    Added returnTo functionality and login/logout

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,16 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to YelpCamp!');
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to YelpCamp!');
+            res.redirect('/campgrounds');
+        })
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register')
@@ -27,6 +30,14 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl)
+})
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', 'Successfully logged out!');
     res.redirect('/campgrounds')
 })
 
