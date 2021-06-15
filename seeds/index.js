97ce6d2d9fa62b1cbcf42525762f95d111440c69
Erik Dever <erikdever@hotmail.com>
2021-06-14 22:39:37 -0600

    Added post and review authorization and permissions

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '60c2b85d934641692c0ceca1',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
