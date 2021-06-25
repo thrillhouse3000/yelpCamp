29d5189e1a1372f7c65dfb55f274687ddcbe1c1f
Erik Dever <erikdever@hotmail.com>
2021-06-24 20:30:39 -0600

    Refactored routes, added route controllers and route grouping

--- middleware.js
+++ middleware.js
@@ -44,7 +44,7 @@ module.exports.validateReview = (req, res, next) => {
 
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { reviewId } = req.params;
-    const review = await Review.findById(id);
+    const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
