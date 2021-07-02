333125c522f38c99276118223d9c0ead8ce40c99
Erik Dever <erikdever@hotmail.com>
2021-07-01 22:06:29 -0600

    Added cloudinary and image uploads

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,13 +4,15 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utilities/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' })
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
+
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
